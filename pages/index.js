8352f0b8fdd239b1378093436f02a9a50ef5e78d
Roel Verbunt <roelv@fastmail.com>
2021-08-09 11:51:36 +0200

    Add links using NextJS Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 function HomePage() {
     return (
     <div>
         <h1>Home Page</h1>
+        <ul>
+            <li><Link href='/products'>Products Index</Link></li>
+            <li><Link href='/products/15'>Product 15</Link></li>
+        </ul>
     </div>
     );
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 function ProductsHomePage() {
+    const allProducts = [
+        { id: 'x01', name: 'Toaster'},
+        { id: 'plt', name: 'Plate'},
+        { id: 'BIG', name: 'Big Mac'}
+    ];
     return (
     <div>
         <h1>Products Home Page</h1>
+        <h3>Our products catalog:</h3>
+        <ul>
+        { allProducts.map(prod => 
+            <li key={prod.id}><Link href={`/products/${prod.id}`}>{prod.name}</Link></li>)}
+        </ul>
     </div>
     );
 }
