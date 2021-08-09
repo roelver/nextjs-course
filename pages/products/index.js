0e3e47a6daaf02fb777a069fb6d04c72075bf6cf
Roel Verbunt <roelv@fastmail.com>
2021-08-09 12:00:49 +0200

    Add programmatic links

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,11 +1,19 @@
+import {useRouter} from 'next/router';
 import Link from 'next/link';
 
 function ProductsHomePage() {
+    const router = useRouter();
     const allProducts = [
         { id: 'x01', name: 'Toaster'},
         { id: 'plt', name: 'Plate'},
         { id: 'BIG', name: 'Big Mac'}
     ];
+
+    function handleClick25() {
+        // Programatic links
+        router.push('/products/25');
+    }
+
     return (
     <div>
         <h1>Products Home Page</h1>
@@ -14,6 +22,7 @@ function ProductsHomePage() {
         { allProducts.map(prod => 
             <li key={prod.id}><Link href={`/products/${prod.id}`}>{prod.name}</Link></li>)}
         </ul>
+        <button onClick={handleClick25}>Goto Product 25</button>
     </div>
     );
 }
